1d7cfe0e3d3c0354be6ed622e05c8dddf6181ae8
eli.rukhadze <eli.rukhadze@gmail.com>
2026-05-08 14:45:41 +0200

    feat: configure unit testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{ts,tsx}',
+        'src/**/*.spec.{ts,tsx}',
+        'src/main.tsx',
+        'src/tests/setup.ts',
+        'src/**/*.d.ts',
+      ],
+
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
